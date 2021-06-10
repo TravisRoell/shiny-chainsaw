b603bef1a3bf77c64357b82d2778e10c81923401
TravisRoell <travisroelltx@gmail.com>
2021-06-10 00:53:33 -0500

    First commit

---
+++ Level-1-Beginner/src/main.js
@@ -0,0 +1,118 @@
+// adds 2 to inputted number
+// ex: addTwo(5); -> 7
+// ex: addTwo(6); -> 8
+function addTwo(number) {
+	return number + 2;
+}
+
+
+// adds the two inputted numbers together
+// ex: add(5, 10); -> 15
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+
+// adds all three inputted numbers together
+// ex: addd(1,2,3); -> 6
+// ex: addd(2,4,2); -> 8
+function addd(value1, value2, value3) {
+  return value1 + value2 + value3;
+}
+
+
+// subtracts the second inputted number from the first inputted number
+// ex: subtract(5, 10); -> -5
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+
+// multiplies the inputted number by 2
+// ex: multiplyByTwo(5); -> 10
+// ex: multiplyByTwo(6); -> 12
+function multiplyByTwo(numero) {
+  return numero * 2;
+}
+
+
+// multplies the two inputted numbers together
+// ex: multiply(1,2); -> 2
+// ex: multiply(2,2); -> 4
+function multiply(val1, val2) {
+  return val1 * val2;
+}
+
+
+// multiplies all three inputted numbers together
+// ex: multiplyy(1,2,3); -> 6
+// ex: multiplyy(2,2,3); -> 12
+function multiplyy(a, b, c) {
+  return a * b * c;
+}
+
+
+// returns the square of the inputted number
+// ex: square(5); -> 25
+function square(value) {
+  return Math.pow(value,2);
+}
+
+
+// determines if the number is odd
+// outputs true if the number is odd
+// outputs false if the number is even
+// ex: isOdd(4); -> false
+// ex: isOdd(5); -> true
+function isOdd(num) {
+  return (num % 2) == 1;
+}
+
+
+// determines if the number is negative
+// outputs true if the number is negative
+// outputs false if the number is zero or positive
+// ex: isNegative(-1); -> true
+// ex: isNegative(1); -> false
+// ex: isNegative(0); -> false
+function isNegative(num) {
+  return num < 0;
+}
+
+
+// returns the absolute value of the inputted number
+// ex: positive(-1); -> 1
+// ex: positive(1); -> 1
+function positive(num) {
+  return Math.abs(num);
+}
+
+// Returns boolean of whether argument is classified as a Number object
+// isNumber(5); → true
+// isNumber('hi'); → false
+function isNumber(value) {
+  return Number.isInteger(value);
+}
+
+// Returns boolean of whether argument is classified as an Array object
+// isArray(5); → false
+// isArray([1,2,3]); → true
+function isArray(value) {
+  return Array.isArray(value);
+
+}
+
+// Returns boolean of whether argument is classified as an Object
+// isObject(5); → false
+// isObject([1,2,3]); → true
+function isObject(value) {
+  return value === Object(value);
+}
+
+// return boolean of whether argument is classified as null
+// isNull(null); -> true
+// isNull(5); -> false
+function isNull(value) {
+  return value === null;
+}
+
---
+++ Level-2-Intermediate/src/main.js
@@ -0,0 +1,45 @@
+// this function accepts an array of numbers
+// and returns an array of only the odd numbers
+// ex: returnOdds([1,2,3,4,5,6,7]); -> [1,3,5,7]
+function returnOdds(array) {
+  // CODE HERE
+
+}
+
+
+// this function accepts an array of numbers
+// and returns an array of only the even numbers
+// ex: returnEvent([1,2,3,4,5,6,7]); -> [2,4,6]
+function returnEvens(array) {
+  // CODE HERE
+
+}
+
+
+// returns only the max element from the inputted array of numbers
+// ex: findMax([1,25,6,3]); -> 25
+function findMax(array) {
+  // CODE HERE
+
+}
+
+/**
+ * remove leading and trailing whitespace or specified characters from string
+ * trim(' hello '); -> 'hello'
+ */
+function trim(string) {
+  // CODE HERE
+}
+
+// under the hood, a JavaScript array is a specific type of object in which values are paired with sequentially numbered keys.
+// the "Array" object also contains a number of methods that give arrays their functionality.
+// the below function should return an empty object that has the behavior and functionality of an array. this object should have the following methods:
+  // push(val) adds val to the end
+  // pop() removes a value from the end and returns it
+  // unshift(val) adds val to the beginning
+  // shift() removes a value from the beginning and returns it
+// the goal of this problem is to reverse engineer what array methods are actually doing and create an object that has those methods
+function createArray() {
+  // CODE HERE
+
+}
